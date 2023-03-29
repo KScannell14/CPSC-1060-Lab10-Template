3914ae9d9f5e4945b5e6b6878d838fe8a2c11ce9
Kyle Sanders Scannell <kscanne@babbage14.computing.clemson.edu>
2023-03-29 10:40:35 -0400

    test1

--- IceCream.java
+++ IceCream.java
@@ -6,6 +6,7 @@ public class IceCream {
     private double pricePerScoop;
     private boolean isDeluxeBrand;
     private ArrayList<Topping> toppings;
+    public double totalPrice;
 
     public boolean validateFlavor(String flavor) {
         return flavor.equalsIgnoreCase("Vanilla") || 
@@ -13,5 +14,50 @@ public class IceCream {
             flavor.equalsIgnoreCase("Strawberry");
     }
 
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+        switch flavor {
+                case flavor.equalsIgnoreCase("Vanilla"):
+                    pricePerScoop = 1.05;
+                break;
+
+                case flavor.equalsIgnoreCase("Chocolate"):
+                    pricePerScoop = 1.12;
+                break;
+
+                case flavor.equalsIgnoreCase("Strawberry");
+                    pricePerScoop = 1.32;
+                break;
+            }
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setDeluxeBrand(boolean isDeluxeBrand) {
+        this.isDeluxeBrand = isDeluxeBrand;
+    }
+
+    public boolean getDeluxeBrand() {
+        return isDeluxeBrand;
+    }
+
+    public void setNumScoops(int numScoops) {
+        this.numScoops = numScoops;
+    }
+
+    public int getNumScoops() {
+        return numScoops;
+    }
+
+    public void calcTotal() {
+        if (isDeluxeBrand == true) {
+            totalPrice = numScoops * pricePerScoop * 1.42;
+        }
+        else {
+            totalPrice = numScoops * pricePerScoop;
+        }
+    }
 
 }
